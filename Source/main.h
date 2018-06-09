8ea3fb302070bffb43c71cc87fe6c632eda15c72
brianwong1103 <brianandivan@gmail.com>
2018-06-09 16:13:11 +0800

    Source Of First Version

---
+++ Source/main.h
@@ -0,0 +1,8 @@
+#define CM_FILE_SAVEAS	9072
+#define CM_FILE_EXIT	9071
+#define CM_FILE_OPEN	9070
+#define CM_ABOUT        9069
+#define CM_LINK         9068
+#define CM_DLLWS        9067
+
+#define IDC_MAIN_TEXT 1001
